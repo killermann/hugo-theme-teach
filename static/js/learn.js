1bc14bdedfc66210f6bf47583fe462d63131b0e2
Sam Killermann <sK@hues.xyz>
2019-06-11 16:52:53 -0500

    ...

--- static/js/learn.js
+++ static/js/learn.js
@@ -132,12 +132,12 @@ jQuery(document).ready(function() {
     jQuery(document).keydown(function(e) {
       // prev links - left arrow key
       if(e.which == '37') {
-        jQuery('.nav.nav-prev').click();
+        jQuery('.nav .nav-prev').click();
       }
 
       // next links - right arrow key
       if(e.which == '39') {
-        jQuery('.nav.nav-next').click();
+        jQuery('.nav .nav-next').click();
       }
     });
 
