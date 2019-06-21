90491ae89fbef612bd0720f1f3c885cdf278a591
Sam Killermann <sK@hues.xyz>
2019-06-21 10:10:19 -0500

    unicode, obvs

--- static/js/teach.js
+++ static/js/teach.js
@@ -72,7 +72,7 @@ InstantClick.on('change', function() { // Replaces jQuery(window).on('load', fun
 
     jQuery('input, textarea').keydown(function (e) {
          //  left and right arrow keys
-         if (e.which == '37' || e.which == '39' || e.which == 'd') {
+         if (e.which == '37' || e.which == '39' || e.which == '68') {
              e.stopPropagation();
          }
      });
@@ -87,7 +87,7 @@ InstantClick.on('change', function() { // Replaces jQuery(window).on('load', fun
         jQuery('.nav .nav-next').click();
       }
       // next links - right arrow key
-      if(e.which == 'd') {
+      if(e.which == '68') {
         jQuery('.nav .discourse-link').click();
       }
     });
