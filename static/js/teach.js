ee44d091cef125da164343b53b9a06b98ee31ff3
Sam Killermann <sK@hues.xyz>
2019-06-21 19:47:51 -0500

    ...

--- static/js/teach.js
+++ static/js/teach.js
@@ -207,9 +207,4 @@ InstantClick.on('change', function() { // Replaces jQuery(window).on('load', fun
     for (var url in localStorage) {
         if (localStorage.getItem(url) == 1) jQuery('[data-nav-id="' + url + '"]').addClass('visited');
     }
-
-    // Typeform
-
-    (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
-
 });
